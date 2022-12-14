01c81503855b63bad98c45e0546376cf58094693
VICT0R89 <victor4guiar89@gmail.com>
2022-12-14 14:39:56 -0300

    eCommerce first

---
+++ data.js
@@ -0,0 +1,57 @@
+let shopItemsData = [
+  {
+    id: "item001",
+    name: "Mystic Ultima sword",
+    price: 950,
+    description: "The best sword for your quest! This sword has a mysterious power",
+    img: "img/img-1.png"
+  },
+  {
+    id: "item002",
+    name: "Mystic Blue sword",
+    price: 350,
+    description: "The mystical blue sword contains the power of the ocean!",
+    img: "img/img-2.png"
+  },
+  {
+    id: "item003",
+    name: "Mystic Red sword",
+    price: 350,
+    description: "The mystical red sword contains the power of fire!",
+    img: "img/img-3.png"
+  },
+  {
+    id: "item004",
+    name: "Mystic Yellow sword",
+    price: 350,
+    description: "The mystical yellow sword contains the power of thunder!",
+    img: "img/img-4.png"
+  },
+  {
+    id: "item005",
+    name: "Mystic Green sword",
+    price: 350,
+    description: "The mystical green sword contains the power of wind!",
+    img: "img/img-5.png"
+  },
+  {
+    id: "item006",
+    name: "Mystic Poison sword",
+    price: 450,
+    description: "The poisonous mystical sword can cause poison in your enemies!",
+    img: "img/img-6.png"
+  },
+  {
+    id: "item007",
+    name: "Mystic Black sword",
+    price: 750,
+    description: "The mystical black sword contains the power of darkness!",
+    img: "img/img-7.png"
+  },
+  {
+    id: "item008",
+    name: "Mystic White sword",
+    price: 750,
+    description: "The mystical white sword contains the power of light!",
+    img: "img/img-8.png"
+  },];
